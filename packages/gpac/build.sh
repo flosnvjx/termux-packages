a41eca833c28d9de8fbfb6383ee3edcf49c7bf5f
flos <flosnvjx@users.noreply.github.com>
2025-12-15 22:46:17 +0800

    t

--- packages/gpac/build.sh
+++ packages/gpac/build.sh
@@ -2,21 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://gpac.wp.imt.fr/
 TERMUX_PKG_DESCRIPTION="An open-source multimedia framework focused on modularity and standards compliance"
 TERMUX_PKG_LICENSE="LGPL-2.1-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.4.0"
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=99c8c994d5364b963d18eff24af2576b38d38b3460df27d451248982ea16157a
+TERMUX_PKG_VERSION="2.4.0r1"
+TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/refs/tags/abi-12.21.tar.gz
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_DEPENDS="ffmpeg, freetype, liba52, libjpeg-turbo, liblzma, libmad, libnghttp2, libogg, libpng, libtheora, libvorbis, openjpeg, openssl, pulseaudio, xvidcore, zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="STRIP=:"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11"
 
 termux_step_pre_configure() {
 	declare -a _commits=(
-	18863aa2
+	4e9091c4568e4574eeaf8714256d20a916735603
 	)
 
 	declare -a _checksums=(
-	3a4e10a031bc081a402bfd24889f85fcbf99d4fd36a9086aedda546445037bec
+	SKIP_CHECKSUM
 	)
 
 	for i in "${!_commits[@]}"; do
