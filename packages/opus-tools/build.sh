03d4683d1f973a2c6bf287b771801498245a611f
flos <flosnvjx@users.noreply.github.com>
2026-01-31 11:09:43 +0800

    bst

--- packages/opus-tools/build.sh
+++ packages/opus-tools/build.sh
@@ -6,4 +6,13 @@ TERMUX_PKG_VERSION=0.2
 TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://downloads.xiph.org/releases/opus/opus-tools-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b4e56cb00d3e509acfba9a9b627ffd8273b876b4e2408642259f6da28fa0ff86
-TERMUX_PKG_DEPENDS="libopusenc, libogg, libflac, opusfile, libopus"
+TERMUX_PKG_BUILD_DEPENDS="libopusenc-static, libopusenc, libogg-static, libogg, opusfile-static, opusfile, libopus-static, libopus"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-flac"
+
+termux_step_pre_configure() {
+	export OGG_LIBS="-Wl,-Bstatic -logg -Wl,-Bdynamic"
+	export OPUS_LIBS="-Wl,-Bstatic -lopus -Wl,-Bdynamic"
+	export OPUSFILE_LIBS="-Wl,-Bstatic -lopusfile -Wl,-Bdynamic"
+	export OPUSURL_LIBS="-Wl,-Bstatic -lopusurl -Wl,-Bdynamic"
+	export LIBOPUSENC_LIBS="-Wl,-Bstatic -lopusenc -Wl,-Bdynamic"
+}
